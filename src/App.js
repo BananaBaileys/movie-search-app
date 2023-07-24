44d5464df4937ff514cddb1dd24a9940bee1a958
BananaBaileys <tian987654321@gmail.com>
2023-07-24 11:43:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { useEffect } from 'react';
 
+
+import './App.css';
+import SearchIcon from './search.svg';
+
 //27566bf4
 
 const API_URL = 'http://www.omdbapi.com?apikey=27566bf4'
@@ -19,9 +23,20 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App
-
-        </h1>
+        <div className="app">
+            <h1>Find Your Movie</h1>        {/* title */}
+            <div className="search">
+                <input 
+                    placeholder = "Search for movies"
+                    value="superman"
+                    onChange={() => {}}
+                    />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                />
+            </div>
+        </div>
     );
 }
 
