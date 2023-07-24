57d151b254246fecc93d2d07c1885c8927b412fc
BananaBaileys <tian987654321@gmail.com>
2023-07-24 11:30:45 -0400

    base update
    title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//27566bf4
+
+const API_URL = 'http://www.omdbapi.com?apikey=27566bf4'
+
+const App = () => {
+
+    // to fetch movies
+    const searchMovies = async (title) => {
+        const response = await fetch('${API_URL}&s=${title}');  //call api  
+        const data = await response.json();     // get the data from it
+
+        console.log(data.search);
+    }
+    useEffect(() => {
+        searchMovies('Spiderman');
+
+    }, []);
+
+    return (
+        <h1>App
+
+        </h1>
+    );
 }
 
 export default App;
