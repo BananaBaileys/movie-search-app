ed7f381666606f43f33a801707b2a746792fdf24
BananaBaileys <tian987654321@gmail.com>
2023-07-25 23:24:49 -0400

    partial update
    title

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=27566bf4'
 
+const movie1 = { /*movie example*/
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imbdID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
 const App = () => {
 
     // to fetch movies
@@ -34,8 +42,30 @@ const App = () => {
                 <img
                     src={SearchIcon}
                     alt="search"
+                    /* magnifing icon */
+                    onClick={() => {}}
+                    /* also has a onClick property to this image */
                 />
             </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     );
 }
